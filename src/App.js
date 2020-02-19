41ca7e54082b9288efa63c60b7d124e9b0da921d
Jideofor Nwoke <njusticej@gmail.com>
2020-02-19 17:44:26 +0100

    add updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Homepage from './components/Homepage'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+        <Route exact path='/'>
+          <Homepage />
+        </Route> 
+        <Route path='/signup'>
+          <Signup />
+        </Route> 
+        </Switch>
+      </Router>
     </div>
   );
 }
