4d1928a78cc5056520af2ce214d7485a94ed7a5f
Jideofor Nwoke <njusticej@gmail.com>
2020-02-19 17:41:46 +0100

    add Homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Body from './Body';
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Body/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Homepage;
