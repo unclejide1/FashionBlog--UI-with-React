2503260cff83101c69e5c812827cba9c14bf0cfb
Jideofor Nwoke <njusticej@gmail.com>
2020-02-19 17:41:07 +0100

    add Signup component

---
+++ src/components/Signup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Signup = () => {
+    return (
+        
+        <div>
+            <Navbar/>
+            <form>
+                <div>
+
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Signup;
